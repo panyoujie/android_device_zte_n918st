2bcaab95da8b1c066061975e4f8a840bc35c4c7e
panyoujie <1724962375@qq.com>
2016-01-30 17:21:07 +0800

    n918st: Fix wlan symbolic is not correct

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -21,7 +21,7 @@ include $(CLEAR_VARS)
 
 #Create symbolic links
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
-        ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
+        ln -sf /persist/WCNSS_qcom_cfg.ini \
         $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini; \
         ln -sf /persist/WCNSS_qcom_wlan_nv.bin \
         $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin)
